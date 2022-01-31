730ea2692cb15d7d7d6cd5befaaa4eccaab9b51b
And-Salei <saleiandrew@gmail.com>
2022-02-01 01:09:47 +0200

    Start

---
+++ assets/app.js
@@ -0,0 +1,44 @@
+const alphabet = {
+    'A': 'Alfa',
+    'B': 'Bravo',
+    'C': 'Charlie',
+    'D': 'Delta',
+    'E': 'Echo',
+    'F': 'Foxtrot',
+    'G': 'Golf',
+    'H': 'Hotel',
+    'I': 'India',
+    'J': 'Juliet',
+    'K': 'Kilo',
+    'L': 'Lima',
+    'M': 'Mike',
+    'N': 'November',
+    'O': 'Oscar',
+    'P': 'Papa',
+    'Q': 'Quebec',
+    'R': 'Romeo',
+    'S': 'Sierra',
+    'T': 'Tango',
+    'U': 'Uniform',
+    'V': 'Victor',
+    'W': 'Whiskey',
+    'X': 'X-ray',
+    'Y': 'Yankee',
+    'Z': 'Zulu',
+}
+
+const number1 = {
+    '1': 'One',
+    '2': 'Two',
+    '3': 'Three',
+    '4': 'Fouth',
+    '5': 'Five',
+    '6': 'Six',
+    '7': 'Seven',
+    '8': 'Eight',
+    '9': 'Nine',
+    '0': 'Zero',
+}
+
+let text = 'AC5331'
+console.log (alphabet[text[0]]);
